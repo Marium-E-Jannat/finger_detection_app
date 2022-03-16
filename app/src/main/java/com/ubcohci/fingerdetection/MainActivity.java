244037d10693307323a9c49c177ae2bba881109a
Thuan Vo <thuan.votann@gmail.com>
2022-03-16 16:44:42 -0700

    Add analyzer for image frames

--- app/src/main/java/com/ubcohci/fingerdetection/MainActivity.java
+++ app/src/main/java/com/ubcohci/fingerdetection/MainActivity.java
@@ -1,7 +1,9 @@
 package com.ubcohci.fingerdetection;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.camera.core.CameraSelector;
+import androidx.camera.core.ImageProxy;
 import androidx.camera.core.Preview;
 import androidx.camera.lifecycle.ProcessCameraProvider;
 import androidx.core.app.ActivityCompat;
@@ -9,6 +11,7 @@ import androidx.core.content.ContextCompat;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 
 import com.google.common.util.concurrent.ListenableFuture;
 import com.ubcohci.fingerdetection.camera.CameraSource;
@@ -20,24 +23,21 @@ import org.jetbrains.annotations.NotNull;
 import java.util.concurrent.Executor;
 import java.util.concurrent.Executors;
 
-public class MainActivity extends AppCompatActivity implements ActivityCompat.OnRequestPermissionsResultCallback {
+public class MainActivity extends AppCompatActivity
+        implements ActivityCompat.OnRequestPermissionsResultCallback, CameraSource.AnalyzerListener  {
     // Tags and request codes
     private static final int PERMISSION_REQUESTS = 1;
     private static final String TAG = "MainActivity";
 
     // View related objects
-    private ActivityMainBinding viewBinding;
-    private GraphicOverlay graphicOverlay;
+    public ActivityMainBinding viewBinding;
+    public GraphicOverlay graphicOverlay;
 
     // Camera configuration
     private CameraSource cameraSource;
-    private ListenableFuture<ProcessCameraProvider> cameraProviderFuture;
-    private Executor cameraExecutor;
 
     // Permission manager
     private PermissionManager permissionManager;
-
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,79 +46,19 @@ public class MainActivity extends AppCompatActivity implements ActivityCompat.On
         viewBinding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(viewBinding.getRoot());
 
-        cameraExecutor = Executors.newSingleThreadExecutor();
 
         // Get the graphic overlay
         graphicOverlay = viewBinding.graphicOverlay;
-
-        // Set listeners
-        viewBinding.imageCaptureButton.setOnClickListener(
-                view -> {
-
-                }
-        );
-
-        viewBinding.videoCaptureButton.setOnClickListener(
-                view -> {
-
-        });
+        cameraSource = new CameraSource(TAG, graphicOverlay, this, this);
 
         // Check for permissions
         permissionManager = new PermissionManager(TAG,this, PERMISSION_REQUESTS);
 
         if (permissionManager.isAllPermissionsGranted()) {
-                startCamera();
+                cameraSource.startCamera();
         } else {
             permissionManager.getRuntimePermissions();
         }
-    }
-
-    public void startCamera() {
-        cameraProviderFuture = ProcessCameraProvider.getInstance(this);
-        cameraProviderFuture.addListener(
-                () -> {
-                    try {
-                        ProcessCameraProvider cameraProvider = cameraProviderFuture.get();
-
-                        // Preview
-                        Preview preview = new Preview.Builder().build();
-                        preview.setSurfaceProvider(viewBinding.viewFinder.getSurfaceProvider());
-
-                        // Camera selector
-                        CameraSelector selector = CameraSelector.DEFAULT_FRONT_CAMERA;
-
-                        // Unbind use cases before rebinding
-                        cameraProvider.unbindAll();
-
-                        // Bind use cases
-                        cameraProvider.bindToLifecycle(
-                                this,
-                                selector,
-                                preview
-                        );
-                    } catch (Exception e) {
-                        Log.e(TAG, e.getMessage());
-                    }
-                },
-                ActivityCompat.getMainExecutor(this) // Running on the main thread
-        );
-    }
-
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-    }
-
-    @Override
-    protected void onResume() {
-        startCamera();
-        super.onResume();
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
     }
 
     @Override
@@ -132,5 +72,13 @@ public class MainActivity extends AppCompatActivity implements ActivityCompat.On
             }
         }
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    }
+
+    @Override
+    public void handle(@NonNull ImageProxy image) {
+        image.getPlanes()[0].getBuffer();
+
+        Log.d(TAG, String.format("Height: %d\tWidth:%d", image.getHeight(), image.getWidth()));
+        image.close();
     }
 }
