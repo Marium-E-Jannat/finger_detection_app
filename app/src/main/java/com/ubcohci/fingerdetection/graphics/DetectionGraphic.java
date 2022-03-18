6f61e69d364b3bfe53daba8d572bd27e8c6bde20
Thuan Vo <thuan.votann@gmail.com>
2022-03-18 16:34:35 -0700

    Restyle model result paintings

--- app/src/main/java/com/ubcohci/fingerdetection/graphics/DetectionGraphic.java
+++ app/src/main/java/com/ubcohci/fingerdetection/graphics/DetectionGraphic.java
@@ -37,30 +37,6 @@ public class DetectionGraphic extends GraphicOverlay.Graphic {
     @Override
     public void draw(Canvas canvas) {
         Log.d(TAG, String.format(Locale.CANADA, "name: %s\tid: %s", info.className, info.classId));
-
-        // Get canvas width
-        int width = canvas.getWidth();
-
-        // Set paint
-        Paint backgroundPaint = new Paint();
-        backgroundPaint.setColor(Color.WHITE);
-
-        // Draw background
-        canvas.drawRect(
-                new Rect(width * 2 / 3, 10, width - 10,
-                        (int) DrawingUtils.convertFromDpToPx(100f, context.getResources().getDisplayMetrics())),
-                backgroundPaint
-        );
-
-        // Draw text
-        TextPaint textPaint = new TextPaint();
-        textPaint.setColor(Color.BLACK);
-        textPaint.setTextSize(DrawingUtils.convertFromSpToPx(24f, context.getResources().getDisplayMetrics()));
-        canvas.drawText(
-                String.format(Locale.CANADA, "Name: %s\nID: %s", info.className, info.classId),
-                width * 2 / 3f + 10 ,
-                DrawingUtils.convertFromDpToPx(50f, context.getResources().getDisplayMetrics()),
-                textPaint
-        );
+        DrawingUtils.drawModelResult(canvas, context.getResources().getDisplayMetrics(), info);
     }
 }
--- app/src/main/java/com/ubcohci/fingerdetection/graphics/DrawingUtils.java
+++ app/src/main/java/com/ubcohci/fingerdetection/graphics/DrawingUtils.java
@@ -1,9 +1,32 @@
 package com.ubcohci.fingerdetection.graphics;
 
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Rect;
+import android.text.TextPaint;
 import android.util.DisplayMetrics;
 import android.util.TypedValue;
 
+import androidx.annotation.NonNull;
+
+import java.util.Locale;
+
 public class DrawingUtils {
+
+    public static Paint backgroundPaint;
+    public static Paint textPaint;
+
+    static {
+        // Set paint
+        backgroundPaint = new Paint();
+        backgroundPaint.setColor(Color.WHITE);
+
+
+        // Draw text
+        textPaint = new TextPaint();
+        textPaint.setColor(Color.BLACK);
+    }
     public static float convertFromDpToPx(float sizeInDp, DisplayMetrics metrics) {
         return TypedValue.applyDimension(
                 TypedValue.COMPLEX_UNIT_DIP,
@@ -18,5 +41,52 @@ public class DrawingUtils {
                 sizeInSp,
                 metrics
         );
+    }
+
+
+    public static void drawModelResult(@NonNull Canvas canvas, @NonNull  DisplayMetrics displayMetrics, DetectionGraphic.DetectionInfo info) {
+
+        // Set text size here since we don't have access to display_metrics at compile time.
+        textPaint.setTextSize(DrawingUtils.convertFromSpToPx(18f, displayMetrics));
+
+        // Get canvas width
+        int width = canvas.getWidth();
+
+        // Define margins and paddings
+        float margin = convertFromDpToPx(10f, displayMetrics); // 10dp
+        float padding = convertFromDpToPx(5, displayMetrics); // 5dp
+
+        // Define rectangle size
+        float rectWidth = width / 2f;
+        float rectHeight = (textPaint.getTextSize() + padding) * 2 + padding;
+
+
+        // Draw background
+        canvas.drawRect(
+                new Rect(width - (int)rectWidth - (int) margin,
+                        (int) margin,
+                        width - (int) margin,
+                        (int) (margin + rectHeight)),
+                backgroundPaint
+        );
+
+        // Draw class Name
+        canvas.drawText(
+                String.format(Locale.CANADA, "Name: %s", info.className),
+                width - rectWidth -  margin + padding,
+                margin + padding + textPaint.getTextSize(),
+                textPaint
+        );
+        canvas.save();
+        canvas.translate(0, margin + padding + textPaint.getTextSize());
+
+        // Draw id name
+        canvas.drawText(
+                String.format(Locale.CANADA, "ID: %s", info.classId),
+                width - rectWidth -  margin + padding,
+                padding + textPaint.getTextSize(),
+                textPaint
+        );
+        canvas.restore();
     }
 }
