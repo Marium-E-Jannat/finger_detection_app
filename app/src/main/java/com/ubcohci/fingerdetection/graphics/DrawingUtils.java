1a76942611fee2f1ee3e7891f56285cbbee923c9
Thuan Vo <thuan.votann@gmail.com>
2022-03-18 16:16:38 -0700

    Add conversion from dp, sp to px

--- app/src/main/java/com/ubcohci/fingerdetection/graphics/DrawingUtils.java
+++ app/src/main/java/com/ubcohci/fingerdetection/graphics/DrawingUtils.java
@@ -1,5 +1,22 @@
 package com.ubcohci.fingerdetection.graphics;
 
-public class DrawingUtils {
+import android.util.DisplayMetrics;
+import android.util.TypedValue;
 
+public class DrawingUtils {
+    public static float convertFromDpToPx(float sizeInDp, DisplayMetrics metrics) {
+        return TypedValue.applyDimension(
+                TypedValue.COMPLEX_UNIT_DIP,
+                sizeInDp,
+                metrics
+        );
+    }
+
+    public static float convertFromSpToPx(float sizeInSp, DisplayMetrics metrics) {
+        return TypedValue.applyDimension(
+                TypedValue.COMPLEX_UNIT_SP,
+                sizeInSp,
+                metrics
+        );
+    }
 }
