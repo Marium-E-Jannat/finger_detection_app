4236a9ebb51cd5f77015ee33b6b8e32cf606d646
Thuan Vo <thuan.votann@gmail.com>
2022-03-25 00:21:17 -0700

    Initialize video player on create

--- app/src/main/java/com/ubcohci/fingerdetection/application/VideoControlActivity.java
+++ app/src/main/java/com/ubcohci/fingerdetection/application/VideoControlActivity.java
@@ -1,13 +1,20 @@
 package com.ubcohci.fingerdetection.application;
 
 import android.os.Bundle;
+import android.util.Log;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.youtube.player.YouTubeInitializationResult;
+import com.google.android.youtube.player.YouTubePlayer;
 import com.google.android.youtube.player.YouTubePlayerView;
+import com.ubcohci.fingerdetection.BuildConfig;
 import com.ubcohci.fingerdetection.databinding.ActivityVideoControlBinding;
 
-public class VideoControlActivity extends AppCompatActivity {
+public class VideoControlActivity extends AppCompatActivity implements YouTubePlayer.OnInitializedListener {
+
+    // TAGS
+    private static final String TAG = "VideoControlActivity";
 
     // View binding
     ActivityVideoControlBinding viewBinding;
@@ -25,6 +32,22 @@ public class VideoControlActivity extends AppCompatActivity {
         // Get the video player
         youTubePlayerView = viewBinding.ytPlayer;
 
+        // Initialize youtube player
+        youTubePlayerView.initialize(
+                BuildConfig.YOUTUBE_API_KEY,
+                this
+        );
     }
 
+    @Override
+    public void onInitializationSuccess(YouTubePlayer.Provider provider, YouTubePlayer youTubePlayer, boolean b) {
+        Log.d(TAG, "Video player is initialized!");
+        youTubePlayer.loadVideo("HzeK7g8cD0Y");
+        youTubePlayer.play();
+    }
+
+    @Override
+    public void onInitializationFailure(YouTubePlayer.Provider provider, YouTubeInitializationResult youTubeInitializationResult) {
+        Log.d(TAG, "Video player failed to be initialized!");
+    }
 }
