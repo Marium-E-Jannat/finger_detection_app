3a23edef3f42e1ddcc03d281f2d5aadbe2288e51
Thuan Vo <thuan.votann@gmail.com>
2022-04-16 01:34:06 -0700

    Add camera source for multiple camera usage

---
+++ app/src/main/java/com/ubcohci/fingerdetection/camera/MultiCameraSource.java
@@ -0,0 +1,96 @@
+package com.ubcohci.fingerdetection.camera;
+
+import android.content.Context;
+import android.util.Log;
+
+import androidx.camera.core.CameraSelector;
+import androidx.camera.core.ImageAnalysis;
+import androidx.camera.lifecycle.ProcessCameraProvider;
+import androidx.core.app.ActivityCompat;
+import androidx.lifecycle.LifecycleOwner;
+
+import com.google.common.util.concurrent.ListenableFuture;
+
+public class MultiCameraSource implements CameraSource {
+    // Log tag
+    private final String TAG;
+
+    // Owner
+    private final Context context;
+
+    // Listeners
+    private final AnalyzerListener frontListener;
+    private final AnalyzerListener backListener;
+
+    // Camera provider
+    private ListenableFuture<ProcessCameraProvider> cameraProviderListenableFuture;
+
+    /**
+     * Constructor
+     * @param tag TAG of the owner.
+     * @param context The owner (must be instance of LifecycleOwner)
+     * @param frontListener The image listener for front camera
+     * @param backListener The image listener for back camera
+     */
+    public MultiCameraSource(
+            String tag, Context context, AnalyzerListener frontListener, AnalyzerListener backListener ) {
+        TAG = "MultiCameraSource_" + tag;
+        this.context = context;
+        this.frontListener = frontListener;
+        this.backListener = backListener;
+    }
+
+    @Override
+    public void startCamera() {
+        cameraProviderListenableFuture = ProcessCameraProvider.getInstance(this.context);
+        cameraProviderListenableFuture.addListener(
+                () -> {
+                    try {
+                        ProcessCameraProvider cameraProvider = cameraProviderListenableFuture.get();
+
+                        // Unbind all cases
+                        cameraProvider.unbindAll();
+
+                        // Set up for front camera analyzer
+                        ImageAnalysis frontAnalysis = new ImageAnalysis.Builder().build();
+                        frontAnalysis.setAnalyzer(
+                                ActivityCompat.getMainExecutor(this.context),
+                                new Analyzer(frontListener)
+                        );
+
+                        CameraSelector frontSelector = CameraSelector.DEFAULT_FRONT_CAMERA;
+
+                        // Set up for back camera analyzer
+                        ImageAnalysis backAnalysis = new ImageAnalysis.Builder().build();
+                        backAnalysis.setAnalyzer(
+                                ActivityCompat.getMainExecutor(this.context),
+                                new Analyzer(backListener)
+                        );
+
+                        CameraSelector backSelector = CameraSelector.DEFAULT_BACK_CAMERA;
+
+                        // Bind cases
+                        cameraProvider.bindToLifecycle(
+                                (LifecycleOwner) this.context,
+                                frontSelector,
+                                frontAnalysis
+                        );
+
+                        cameraProvider.bindToLifecycle(
+                                (LifecycleOwner) this.context,
+                                backSelector,
+                                backAnalysis
+                        );
+                    } catch (Exception e) {
+                        Log.e(TAG, e.getMessage());
+                    }
+                },
+                ActivityCompat.getMainExecutor(this.context)
+        );
+    }
+
+    @Override
+    public void releaseCamera() {
+
+    }
+}
