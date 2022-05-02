92946a0e4f7960941a4fa016823ad9e2703884cb
Thuan Vo <thuan.votann@gmail.com>
2022-05-02 00:08:10 -0700

    Refactor task resouce to separate class

---
+++ app/src/main/java/com/ubcohci/fingerdetection/tasks/TaskResource.java
@@ -0,0 +1,11 @@
+package com.ubcohci.fingerdetection.tasks;
+
+public class TaskResource {
+    public static final String[] imageURLs = new String[] {
+            "https://news.ok.ubc.ca/wp-content/uploads/2019/03/housing-770.jpg",
+            "https://finance.cms.ok.ubc.ca/wp-content/uploads/sites/73/2021/04/UBC-Okanagan-Engineering-6-research.jpg",
+            "https://students.cms.ok.ubc.ca/wp-content/uploads/sites/90/2019/05/aerial-campus-okanagan-study-abroad-at-ubc-and-exchange-research-abroad-and-1170-wide.jpg"
+    };
+
+    public static int maxVolume = 100; // Initially 100
+}
